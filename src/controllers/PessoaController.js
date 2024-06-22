a7f1cfeb1d94f3483ae9e28c29d77b79f5edf476
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-06-22 19:27:14 -0300

    first commit

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,14 @@
+const Controller = require('./Controller.js');
+const PessoaServices = require('../services/PessoaServices.js');
+
+class PessoaController {
+  static async pegaTodas (req, res) {
+    try {
+      
+    } catch(erro) {
+      // erro
+    }
+  }
+}
+
+module.exports = PessoaController;
