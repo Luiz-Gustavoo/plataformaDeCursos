ff613738104cbef5fbfc0e55e56476579ab3fc82
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-06-23 21:24:00 -0300

    alterar a rota de /pessoas

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const PessoaController = require('../controllers/PessoaController.js');
 
 const router = Router();
-
-router.get('/pessoas', PessoaController.pegaTodas);
+const pessoaController = new PessoaController();
+router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res));
 
 module.exports = router;
