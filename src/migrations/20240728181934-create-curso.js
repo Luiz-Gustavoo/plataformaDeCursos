39f596b6e60182e220288c3210647e3a353a772b
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-08-05 19:49:32 -0300

    Referenciar as tabelas categoria e pessoas

--- src/migrations/20240728181934-create-curso.js
+++ src/migrations/20240728181934-create-curso.js
@@ -19,8 +19,15 @@ module.exports = {
         type: Sequelize.DATEONLY
       },
       docente_id: {
-        
-      }
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'pessoas', key: 'id'}
+      },
+      categoria_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'categorias', key: 'id'}
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
