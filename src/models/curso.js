24040a22389467d9e0505ef759429324703d896e
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-08-06 12:35:05 -0300

    Adicionar associação com os modelos Pessoa e Matricula

--- src/models/curso.js
+++ src/models/curso.js
@@ -13,6 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       Curso.belongsTo(models.Categoria, {
         foreignKey: 'categoria_id'
       });
+      Curso.belongsTo(models.Pessoa, {
+        foreignKey: 'docente_id'
+      });
+      Curso.hasMany(models.Matricula, {
+        foreignKey: 'curso_id'
+      });
     }
   }
   Curso.init({
