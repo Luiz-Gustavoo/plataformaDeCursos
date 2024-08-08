b5b8ba891538284cbb69961c209db47524ad52fc
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-08-07 21:03:56 -0300

    Adicionar o controller de matricula

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -1,14 +1,18 @@
 const {Router} = require("express");
 const PessoaController = require("../controllers/PessoaController.js");
+const MatriculaController = require('../controllers/MatriculaController.js');
 
 const router = Router();
+
 const pessoaController = new PessoaController();
+const matriculaController = new MatriculaController();
 
 router.post("/pessoas", (req, res) => pessoaController.criar(req, res));
 router.get("/pessoas", (req, res) => pessoaController.pegaTodos(req, res));
 router.get("/pessoas/:id", (req, res) => pessoaController.pegaPorID(req, res));
 router.put("/pessoas/:id", (req, res) => pessoaController.atualizar(req, res));
 router.delete("/pessoas/:id", (req, res) => pessoaController.deletar(req, res));
+router.post("/pessoas/:estudanteId/matriculas", (req, res) => matriculaController.criar(req, res));
 
 
 module.exports = router;
