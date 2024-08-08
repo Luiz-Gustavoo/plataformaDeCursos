154839fccd20305b6697cb563b300f934c23b2d7
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-08-08 11:01:51 -0300

    Adicionar o método pegaMatriculasPorEstudante

--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -6,6 +6,11 @@ class PessoaServices extends Services {
   constructor() {
     super('Pessoa');
   }
+  async pegaMatriculasPorEstudante(id) {
+    const estudante = await super.pegaRegistroPorID(id);
+    const listaMatriculas = await estudante.getAulasMatriculadas();
+    return listaMatriculas;
+  }
     
 }
 
