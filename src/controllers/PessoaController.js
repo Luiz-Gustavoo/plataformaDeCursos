6345b1bcbcd239892044dd9d15728517abcbf258
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-08-08 14:00:27 -0300

    Adicionar tratamento de erro e alterar o acesso ao id da URL

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -9,10 +9,16 @@ class PessoaController extends Controller {
   }
 
   async pegaMatriculas(req, res) {
-    const estudanteId = req.params;
+    const {estudanteId} = req.params;
     try {
       const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
-      return res.status(200).json(listaMatriculas);
+    
+      if(!listaMatriculas) {
+        return res.status(404).json({mensagem: "Não foi possível encontrar matrículas para esse aluno"});
+      } else {
+        return res.status(200).json(listaMatriculas);
+      }
+      
     } catch (erro) {
       //erro
     }
