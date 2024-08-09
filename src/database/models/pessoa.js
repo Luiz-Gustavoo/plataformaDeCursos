ff317b55768ed677245b5b5cea7eff32dab895df
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-08-09 19:36:22 -0300

    Adicionar paranoid

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -29,7 +29,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Pessoa',
-    tableName: 'Pessoas'
+    tableName: 'Pessoas',
+    paranoid: true
   });
   return Pessoa;
 };
