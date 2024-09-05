17ab3aee3349f881ecb407b10f1d0ae2dcab2164
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-09-05 14:24:26 -0300

    adicionar pegaTodasAsMatriculas

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -23,6 +23,22 @@ class PessoaController extends Controller {
     }
   }
 
+  async pegaTodasAsMatriculas(req, res) {
+    const {estudanteId} = req.params;
+    try {
+      const listaMatriculas = await pessoaServices.pegaTodasMatriculasPorEstudante(Number(estudanteId));
+
+      if(!listaMatriculas) {
+        return res.status(404).json({mensagem: "Não foi possível encontrar matrículas para esse aluno"});
+      } else {
+        return res.status(200).json(listaMatriculas);
+      }
+
+    } catch (erro) {
+      return res.status(500).json({erro: erro.mensagem});
+    }
+  }
+
   async pegaCursos(req, res) {
     const {docenteId} = req.params;
     try {
