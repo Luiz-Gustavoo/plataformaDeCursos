07bc89c9bbedb1ad857aaad6487e3a3d84fe5a06
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-09-26 21:27:42 -0300

    Adicionar método pegaCursos

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,3 +1,4 @@
+const {Op} = require('sequelize');
 const Controller = require('./Controller.js');
 const CursoServices = require('../services/CursoServices.js');
 
@@ -7,6 +8,26 @@ class CategoriaController extends Controller {
     constructor() {
         super(cursoServices);
     }
+
+    async pegaCursos(req, res) {
+        const {data_inicial, data_final} = req.query;
+        const where = {};
+
+        //se existirem os param, criar uma prop {}
+        data_inicial || data_final ? where.data_inicio = {} : null;
+        // se existir data inicial, adiciona a prop gte com o valor
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+        // se existir data final, idem
+        data_final ? where.data_inicio[Op.lte] = data_final : null;
+
+        try {
+            const listaDeRegistros = await cursoServices.pegaTodosOsRegistros(where);
+            res.status(200).json(listaDeRegistros);
+        } catch (erro) {
+            res.status(500).json({message: erro.message});
+        }
+    }
 }
+
 
 module.exports = CategoriaController;
